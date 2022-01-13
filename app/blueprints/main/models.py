c3a667dff643ccbbbc55cc32f831644ee718315a
dptran <darren-tran@hotmail.com>
2022-01-13 12:59:04 -0800

    fixed main.routes

--- app/blueprints/main/models.py
+++ app/blueprints/main/models.py
@@ -1,9 +1,9 @@
 from app import db
 from datetime import datetime as dt
 # from app import login_manager
-# from flask_login import UserMixin
+from flask_login import UserMixin
 
-class Post(db.Model):
+class Post(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime, default=dt.utcnow)
